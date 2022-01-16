818e22df12462bf61c31d0069e55a0c8bb8a6b5d
Philippe <philippe.vial90@gmail.com>
2022-01-16 21:55:22 +0100

    Add tests for score when multiple strikes is rolled in a row

--- src/test/java/com/example/bowlingtest/BowlingTest.java
+++ src/test/java/com/example/bowlingtest/BowlingTest.java
@@ -67,4 +67,31 @@ public class BowlingTest {
 
         assertEquals(26, game.score());
     }
+
+    @Test
+    void shouldReturnCorrectBonusWhenTwoStrikesIsRolledInARow() {
+        Game game = new Game();
+
+        game.roll(10);
+        game.roll(10);
+        game.roll(9);
+        game.roll(0);
+        game.multipleRolls(16,0);
+
+        assertEquals(57, game.score());
+    }
+
+    @Test
+    void shouldReturnCorrectBonusWhenThreeStrikesIsRolledInARow() {
+        Game game = new Game();
+
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(0);
+        game.roll(9);
+        game.multipleRolls(15,0);
+
+        assertEquals(78, game.score());
+    }
 }
