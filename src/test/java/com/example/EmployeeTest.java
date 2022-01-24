d2b43be5e047569bbbfe1dccfd41d4f42c4e57b6
Philippe <philippe.vial90@gmail.com>
2022-01-24 09:42:32 +0100

    Add test for Employee setID method and refactor code

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -6,14 +6,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 class EmployeeTest {
+    Employee employee = new Employee("123", 100.0);
 
     @Test
     void getIDShouldReturnOneTwoThree() {
-        Employee employee = new Employee("123", 0.0);
-
         var result = employee.getId();
 
         assertThat(result).isEqualTo("123");
     }
 
+    @Test
+    void setIDShouldGiveEmployeeEnteredID() {
+        employee.setId("456");
+
+        assertThat(employee.getId()).isEqualTo("456");
+    }
 }
